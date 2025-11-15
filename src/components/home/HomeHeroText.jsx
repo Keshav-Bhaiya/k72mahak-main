603630d5eeefa22d050d4d28fd34b9dc18a289c0
My <keshavb266@gmail.com>
2025-11-15 18:28:57 +0530

    new commit

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -7,7 +7,6 @@ const HomeHeroText = () => {
       <div className='text-[8vw] uppercase leading-[7vw] items-center justify-center'>
         The spark for
       </div>
-
       <div className='text-[8vw] flex items-center justify-center uppercase leading-[10vw]'>
         All
         <div className='h-[8vw] w-[16vw] rounded-full overflow-hidden bg-black'>
@@ -15,7 +14,6 @@ const HomeHeroText = () => {
         </div>
         things
       </div>
-
       <div className='text-[8vw] uppercase leading-[10vw] items-center justify-center'>
         creative
       </div>
