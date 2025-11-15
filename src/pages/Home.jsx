c151947f5364fc770f891c29c28efef9b61229d6
My <keshavb266@gmail.com>
2025-11-15 18:14:26 +0530

    new commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Video from '../components/home/video.jsx'
+import HomeHeroText from '../components/home/HomeHeroText.jsx'
+import HomebottomText from '../components/home/HomebottomText.jsx'
+
+const Home = () => {
+  return (
+    <div>
+      {/* Full-screen background video */}
+      <div className='h-screen w-screen fixed'>
+        <Video />
+      </div>
+
+      {/* Foreground content */}
+      <div className='h-screen w-screen relative flex flex-col justify-between'>
+        <HomeHeroText />
+        <HomebottomText />
+      </div>
+    </div>
+  )
+}
+
+export default Home
